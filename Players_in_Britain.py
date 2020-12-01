fa3778318bb8037d3e688cd6f9aa3fd728dcf549
Mark Basham <mark.basham@diamond.ac.uk>
2020-12-01 22:07:07 +0000

    Update of Players in Britain

--- Players_in_Britain.py
+++ Players_in_Britain.py
@@ -6,21 +6,8 @@ from matplotlib.ticker import ScalarFormatter
 import matplotlib.colors as colors
 
 # set the filepath and load in a shapefile
-fp = 'geoassets/england_pca_2012.shp'
-england_df = gpd.read_file(fp)
-# check data type so we can see that this is not a normal dataframe, but a GEOdataframe
-
-fp = 'geoassets/wales_pca_2012.shp'
-wales_df = gpd.read_file(fp)
-
-fp = 'geoassets/scotland_pca_2012.shp'
-scotland_df = gpd.read_file(fp)
-
-print(england_df)
-print(scotland_df)
-print(wales_df)
-
-all_df = england_df.append(scotland_df).append(wales_df)
+fp = 'geoassets/Areas.shp'
+all_df = gpd.read_file(fp)
 
 print(all_df)
 
@@ -42,27 +29,28 @@ df['Q3.3'] = df['Q3.3. 15 - 19 years old'].replace('-','0').replace(np.NaN,0).as
 df['Q3.4'] = df['Q3.4. 20 +'].replace('-','0').replace(np.NaN,0).astype(int)
 df['total'] = df['Q3.1']+df['Q3.2']+df['Q3.3']+df['Q3.4']
 df = df[df['UK'] == 1.0]
-df['label'] = df['Q2'].str.split('[0-9]').str[0].str.upper()
+df['name'] = df['Q2'].str.split('[0-9]').str[0].str.upper()
 
-# pull out individuals only
+# seperate personal and institutional data
 df_ind = df[df['total']<=10]
 df_big = df[df['total']>10]
 
-df_ind = df_ind.groupby('label').agg({'Q3.1':['sum'],'Q3.2':['sum'],'Q3.3':['sum'],'Q3.4':['sum'],'total':['sum']})
-df_big = df_big.groupby('label').agg({'Q3.1':['sum'],'Q3.2':['sum'],'Q3.3':['sum'],'Q3.4':['sum'],'total':['sum']})
+# aggregate the data by postcode region
+df_ind = df_ind.groupby('name').agg({'Q3.1':['sum'],'Q3.2':['sum'],'Q3.3':['sum'],'Q3.4':['sum'],'total':['sum']})
+df_big = df_big.groupby('name').agg({'Q3.1':['sum'],'Q3.2':['sum'],'Q3.3':['sum'],'Q3.4':['sum'],'total':['sum']})
 df_ind.columns = df_ind.columns.get_level_values(0)
 df_big.columns = df_big.columns.get_level_values(0)
 
 print(df_ind)
 
-#merge the 2 datasets, for institutanal and individual
-df_ind = pd.merge(all_df, df_ind, on=['label'], how='left')
-df_big = pd.merge(all_df, df_big, on=['label'], how='left')
+#merge the 2 datasets with map data
+df_ind = pd.merge(all_df, df_ind, on=['name'], how='left')
+df_big = pd.merge(all_df, df_big, on=['name'], how='left')
 
 # create figure and axes for Matplotlib
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8), dpi=300)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 8), dpi=300)
 
-# create map
+# create maps
 plt1 = df_ind.plot(cmap='viridis_r',
                    linewidth=0.5,
                    ax=ax1,
@@ -95,10 +83,10 @@ ax1.axis('off')
 ax2.axis('off')
 
 # add a title
-fig.suptitle('Boardgame players in Britain', fontsize=16)
-ax1.set_title('Small Groups', \
+fig.suptitle('Boardgame players in the United Kingdom', fontsize=16)
+ax1.set_title('Personal', \
               fontdict={'fontsize': '14'})
-ax2.set_title('Large Goups', \
+ax2.set_title('Institutional', \
               fontdict={'fontsize': '14'})
 
 # Create colorbar as a legend
